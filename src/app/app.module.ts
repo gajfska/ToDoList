52bd684ad4544e48144469b2e05f84949462cafb
Werka <weronika.gajska@gmail.com>
2020-10-13 20:20:04 +0200

    Adding idea exception to gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import {MatSelectModule} from '@angular/material/select';
       MatFormFieldModule,
       MatInputModule,
       MatSelectModule,
+      MatButtonModule,
       FormsModule
   ],
   providers: [],
