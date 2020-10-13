35a42572693959f25917f63aad34ec448ef31e6a
Werka <weronika.gajska@gmail.com>
2020-10-13 19:35:15 +0200

    creating table

--- src/app/table/table-row/table-row.component.ts
+++ src/app/table/table-row/table-row.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {TaskModel} from '../../shared/task.model';
+import {TaskService} from '../../shared/task.service';
 
 @Component({
   selector: 'app-table-row',
   templateUrl: './table-row.component.html',
   styleUrls: ['./table-row.component.css']
 })
-export class TableRowComponent implements OnInit {
+export class TableRowComponent {
 
-  constructor() { }
+    @Input() task: TaskModel;
+    @Input() index: number;
 
-  ngOnInit(): void {
-  }
+    deleteButtonVisible = false;
+
+    constructor(private taskService: TaskService) {
+    }
+
+    checkboxChange(values: any): void {
+        this.task.done = values.currentTarget.checked;
+        this.taskService.updateTask(this.task);
+    }
+
+    priorityName(priority: number): string {
+        return this.taskService.priorityName(priority);
+    }
+
+    onDelete(): void {
+        this.taskService.deleteTask(this.task.id);
+    }
+
+    mouseEnter(): void {
+        this.deleteButtonVisible = true;
+    }
+
+    mouseLeave(): void {
+        this.deleteButtonVisible = false;
+    }
 
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TaskModel} from '../shared/task.model';
+import {Subscription} from 'rxjs';
+import {TaskService} from '../shared/task.service';
+
+export enum SortOder {
+    ascending,
+    descending,
+    none
+}
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    displayedColumnsKeys: string[] = ['taskName', 'priority', 'done'];
 
-  ngOnInit(): void {
-  }
+    displayedTasks: TaskModel[] = [];
+    allAddedTasks: TaskModel[] = [];
+
+    numberOfRowsToDisplay = 5;
+    private currentPageDisplayed = 0;
+
+    private subscription: Subscription;
+
+    constructor(private taskService: TaskService) {
+    }
+
+    ngOnInit(): void {
+        this.subscription = this.taskService.tasksChangedSubject
+            .subscribe(
+                (tasks: TaskModel[]) => {
+                    this.allAddedTasks = tasks;
+                    this.updateDisplayedDataPage();
+                }
+            );
+
+        this.taskService.initTasks();
+    }
+
+    // MARK: Sorting
+
+    onSort(sortByType: string): void {
+        this.taskService.updateSortingTypes(sortByType);
+    }
+
+    // MARK: Pagination
+
+    goToPreviousPage(): void {
+        if (this.currentPageDisplayed === 0) {
+            return;
+        }
+
+        this.currentPageDisplayed = this.currentPageDisplayed - 1;
+        this.updateDisplayedDataPage();
+    }
+
+    goToNextPage(): void {
+        const pageEnd = this.currentPageDisplayed * this.numberOfRowsToDisplay + this.numberOfRowsToDisplay;
+        if (pageEnd >= this.allAddedTasks.length) {
+            return;
+        }
+
+        this.currentPageDisplayed = this.currentPageDisplayed + 1;
+        this.updateDisplayedDataPage();
+
+    }
+
+    tasksPerPageChanged(): void {
+        this.currentPageDisplayed = 0;
+        this.updateDisplayedDataPage();
+    }
+
+    updateDisplayedDataPage(): void {
+        this.displayedTasks = this.allAddedTasks.slice(this.pageStart(), this.pageEnd());
+    }
+
+    pageStart(): number {
+        return this.currentPageDisplayed * this.numberOfRowsToDisplay;
+    }
+
+    pageEnd(): number {
+        const maxPageEnd = this.pageStart() + this.numberOfRowsToDisplay;
+        return Math.min(maxPageEnd, this.allAddedTasks.length);
+    }
+
+    // MARK: Header
+
+    arrowSymbol(header): string {
+        if (header === this.taskService.sortBy) {
+            return this.taskService.sortOrder === SortOder.ascending ? '↓' : '↑';
+        } else {
+            return '';
+        }
+    }
+
+    headerName(key: string): string {
+        switch (key) {
+            case 'taskName':
+                return 'Task Name';
+            case 'priority':
+                return 'Priority';
+            case 'done':
+                return 'Done';
+            default:
+                return 'Unknown';
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 
 }
