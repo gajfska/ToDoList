e952745207535d6a1e430b065e3c3c82cd2c97c9
Werka <weronika.gajska@gmail.com>
2020-10-13 19:21:27 +0200

    creating form, table and table row components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
+import { TableRowComponent } from './table/table-row/table-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    TableComponent,
+    TableRowComponent
   ],
   imports: [
     BrowserModule,
